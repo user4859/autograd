7392f7ede6c06b74e1521033d19cf5d18b352bb6
Andrew Carter <acarter4859@gmail.com>
2026-08-08 16:46:33 +0100

    Added the ReLu function

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
 import numpy as np
+
+def ReLu(input: np.ndarray) -> np.ndarray:
+    """
+    Applies the ReLu function on a given numpy array \n
+    ReLu is defined as "max(x, 0)"
+
+    Args:
+        input(array): The tensor which the function will be applied to.
+
+    Returns:
+        np.array: The same array inputed, with each item having had the ReLu function appied to it.
+    """
+    return np.maximum(input, 0)
